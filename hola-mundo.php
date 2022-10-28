bc81ad5bb46ad0ce7e2b1a91fa42ecc51248c7c6
amosda <1376920@senati.pe>
2022-10-28 12:37:39 -0500

    Tercer codigo

--- hola-mundo.php
+++ hola-mundo.php
@@ -1,4 +1,14 @@
-<?php 
-	echo "Hola mundo?";
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Hola mundo en php</title>
+</head>
+<body>
+	<?php 
+		echo "Hola Mundo";
+	 ?>
 
- ?>
+</body>
+</html>
